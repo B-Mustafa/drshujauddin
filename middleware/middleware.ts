ed5029b76d70ab0ca72df26c51642b8296c9b329
Mustafa <mustafabhikhpurwala@gmail.com>
2024-02-24 22:57:07 +0530

    fix: few more fixes for production issues

--- middleware/middleware.ts
+++ middleware/middleware.ts
@@ -1,28 +1,28 @@
 import { CustomUser } from "@/app/api/auth/[...nextauth]/options";
 import { getToken } from "next-auth/jwt";
-import { NextResponse , NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function middleware(request:NextRequest){
-    const { pathname } = request.nextUrl
+export async function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl;
 
-    if(pathname == "/admin/login"){
-        return NextResponse.next
+    if (pathname === "/admin/login") {
+        return NextResponse.next();
     }
 
-    const token = await getToken({req:request})
+    const token = await getToken({ req: request });
 
-    // * Protected routes 
+    const adminProtectedRoutes = ["/admin", "/admin/appointment"];
 
-    const adminProtectedRoutes = ["/admin" , "/admin/appointment"]; 
 
-    if(token == null && adminProtectedRoutes.includes(pathname)){
-        return NextResponse.redirect(new URL("/admin/login?error=Login-to-access" , request.url))
+    if (token == null && adminProtectedRoutes.includes(pathname)) {
+        return NextResponse.redirect(new URL("/admin/login?error=Login-to-access", request.url));
     }
 
-    const user:CustomUser|null = token?.user as CustomUser
+    const user: CustomUser | null = token?.user as CustomUser;
 
-    if(adminProtectedRoutes.includes(pathname) && user.role == 'User'){
-        return NextResponse.redirect(new URL("/admin/login", request.url))
+    
+    if (adminProtectedRoutes.includes(pathname) && user?.role === 'User') {
+        return NextResponse.redirect(new URL("/admin/login", request.url));
     }
-
+    return NextResponse.next();
 }
