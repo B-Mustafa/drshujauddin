59599c3c8908c67cbe30c611557d6208f60f46c8
Mustafa Bhikhapurwala <123825821+B-Mustafa@users.noreply.github.com>
2024-03-06 08:19:41 +0000

    fix: made changes in form submission

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -68,7 +68,7 @@ function Contact() {
         }}
       >
         <input
-          className="h-14 px-4 rounded-lg borderBlack bg-dark-background transition-all border border-gray-100  "
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="senderEmail"
           type="email"
           required
@@ -76,7 +76,7 @@ function Contact() {
           placeholder="Your email"
         />
         <textarea
-          className="h-52 my-3 rounded-lg borderBlack p-4 bg-dark-background transition-all border border-gray-10"
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="message"
           placeholder="Your message"
           required
