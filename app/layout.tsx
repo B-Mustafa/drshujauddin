2e425a7274b21c1b54304e16af2be63b13d5be9e
Mustafa <mustafabhikhpurwala@gmail.com>
2024-03-12 16:36:53 +0530

    favicon change try

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,10 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  icons:{
+    icon: "",
+    apple: "",
+  },
   title: "Saifee Homeopathic Clinic",
   description: "Welcome to Saifee Homeopathic Clinic, where Dr. Shujauddin offers holistic healing with personalized care. Discover natural remedies and alternative medicine for optimal wellness.",
   keywords: "homeopathic clinic, holistic healing, Dr. Shujauddin, homeopathy treatment, natural remedies, alternative medicine, personalized care, homeopathic remedies, homeopathic doctor, individualized treatment, wellness, health, vitality, balance, non-invasive therapies, alternative healthcare",
